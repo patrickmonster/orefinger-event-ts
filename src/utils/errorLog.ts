14d1d59d46f81e6f595e1b2ddb2c11f86719f049
Soung jin Ryu <neocat@kakao.com>
2023-08-14 13:53:19 +0900

    Fix. Error

---
+++ src/utils/errorLog.ts
@@ -0,0 +1,9 @@
+import redis from './redis';
+
+export const error = (tag: string, err: any) => {
+    const date = `ERROR:${tag}:${Date.now()}`;
+    redis.set(date, JSON.stringify(err), {
+        EX: 60 * 60 * 24 * 7,
+    });
+    return date;
+};
