f600528f688dae484f00d7012f1e8f35b81e98f9
Soung jin Ryu <neocat@kakao.com>
2023-08-17 11:29:42 +0900

    Chore. 배포전략 수정

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
 # discord-notification-ts
- 
+
+# [방송알리미](https://orefinger.click) API 서비스
+
+서비스 운용
+- AWS ECS
+- NLB
+- RDS
+- Route 53
+- EC2 (Redis)
+
+배포환경
+- gitaction
+- code Pipline (AWS)
+- ECR (AWS Elastic Container Registry)
+
+주기능
+- Rest API (Fastify)
+- Twitch EventSub
+  - POST\] /twitch/event
+  - POST\] /event/twitch
+- Discord Interaction Event
+  - POST\] /api/admin/bot
+
+
+배포전략
+- 이미지 첨부
+
+## 커밋 메시지 컨벤션 💬
+- Feat : 새로운 기능 추가
+- Fix : 버그 수정
+- Hotfix : 급하게 치명적인 버그 수정
+- Docs : 문서 수정
+- Style : 코드 포맷팅, 세미콜론 등의 스타일 수정(코드 자체 수정 X)
+- Refactor : 프로덕션 코드 리팩토링
+- Test : 테스트 코드, 테스트 코드 리팩토링
+- Chore : 빌드 과정 또는 보조 기능(문서 생성 기능 등) 수정
+- Rename : 파일 혹은 폴더명을 수정하거나 옮기는 작업만인 경우
+- Remove : 파일을 삭제하는 작업만 수행한 경우
+- Comment : 필요한 주석 추가 및 변경
+## 특수성 메세지 컨벤션 💬
+- Dev. 개발배포 (develop 브런치)
+
--- release.bat
+++ release.bat
@@ -1,26 +1,49 @@
 @echo off
+@REM USE EUC-KR
+
 @REM 새로운 릴리즈 제작을 위한 배치 파일입니다.
 
-
-@REM 1. package.json의 버전을 확인합니다.
-@REM 2. 버전을 입력 받습니다.
-@REM 3. 변경된 버전을 커밋합니다.
-@REM 4. 변경된 버전을 태그합니다.
-@REM 5. 커밋된 버전을 푸시합니다.
-@REM 6. 'release/버전'으로 새로운 브런치를 생성합니다
-@REM 7. 새로운 브런치를 푸시합니다.
-
+@REM 1. 현재 브런치가 feat/* 인지 확인합니다.
+@REM 2. 현재 브런치를 develop로 변경합니다.
+@REM 3. feat/* 브런치를 develop 브런치로 머지합니다.
+@REM 4. package.json의 버전을 확인합니다.
+@REM 5. 버전을 입력 받습니다.
+@REM 6. release/* 브런치를 생성합니다.
+@REM 7. release/* 브런치로 checkout합니다.
+@REM 8. 변경된 버전을 적용합니다.
+@REM 9. 변경된 버전을 커밋합니다.
+@REM 10. 변경된 버전을 태그합니다.
+@REM 11. 커밋된 버전을 푸시합니다.
+@REM 12. develop 브런치로 checkout합니다.
+@REM 13. release/* 브런치를 develop 브런치로 머지합니다.
+@REM 14. develop 브런치를 push 합니다.D
+@REM 15. 원래 브런치로 checkout합니다.
 
 FOR /F "tokens=1-4 delims=- " %%i IN ('date /t') DO SET yyyymmdd=%%i%%j%%k
 
 @REM 현재 브런치 정보를 가져옵니다.
-(for /f "tokens=* USEBACKQ" %%a in (`git branch --show-current`) do set branch=%%a)
+(for /f "tokens=* USEBACKQ" %%a in (`git branch --show-current`) do set featBranch=%%a)
 
-echo 가장 최근 반영된 버전을 가져옵니다.
-git checkout master
+@REM 현재 브런치 정보 확인
+IF NOT %featBranch:~0,5%==feat/ (
+    echo 현재 브런치가 feat/* 브런치가 아닙니다.
+    echo 브런치를 feat/* 브런치로 변경 후 다시 시도해주세요.
+    exit /b
+)
+
+@REM 현재 커밋 적용
+echo 현재 브런치를 커밋합니다.
+git push
+
+@REM 개발 브런치로 체크아웃
+echo 개발 브런치로 체크아웃합니다.
+git checkout develop
 git pull
 
+@REM =============================================================================================
+
 @REM 현재 패키지 버전을 확인합니다.
+echo 최신 반영 버전 정보를 확인합니다.
 (for /f "tokens=* USEBACKQ" %%a in (`node -p "require('./package').version"`) do set version=%%a)
 
 (for /F "tokens=1,2,3 delims=." %%a in ("%version%") do (
@@ -28,6 +51,8 @@ git pull
     set minor=%%b
     set patch=%%c
 ))
+
+@REM =============================================================================================
 
 echo ======================
 echo 버전 업데이트 설정 - %version%
@@ -40,20 +65,30 @@ echo ======================
 set /p idx=버전 업데이트 설정을 입력하세요:
 
 if %idx%==1 (
+    @REM 주요 업데이트
     set /a major+=1
     set minor=0
     set patch=0
 ) else if %idx%==2 (
+    @REM 기능업데이트
     set /a minor+=1
     set patch=0
 ) else if %idx%==3 (
+    @REM hotFix
     set /a patch+=1
 ) else (
     echo 버전 업데이트를 취소합니다. - 브런치 롤백
     @REM 롤백후 원위치
-    git checkout %branch%
+    git checkout %featBranch%
     exit /b
 )
+
+@REM =============================================================================================
+
+echo 배포를 위한 환경을 제작합니다.
+git branch release/%version%
+git checkout release/%version%
+echo 배포를 위한 환경을 제작 완료
 
 echo 패키지의 버전을 수정하는중....
 @REM 그냥 실행하면 프로세서가 종료됨
@@ -66,18 +101,7 @@ echo 패키지 버전 수정 완료
 (for /f "tokens=* USEBACKQ" %%a in (`node -p "require('./package').version"`) do set version=%%a)
 echo 업데이트 버전: %version%
 
-timeout 2 > NUL
-
-echo 현재 변경사항을 커밋합니다.
-@REM 변경사항을 커밋하고 진행해야함.
-git push 
-
-echo 배포를 위한 환경을 제작합니다.
-git branch release/%version%
-git checkout release/%version%
-echo 배포를 위한 환경을 제작 완료
-
-timeout 2 > NUL
+@REM =============================================================================================
 
 echo 배포를 위한 환경을 커밋합니다.
 git add package.json
@@ -90,16 +114,13 @@ git tag release-%version%
 git push origin release/%version%
 echo 배포를 위한 환경을 푸시 완료
 
-timeout 2 > NUL
 
-echo 원래 브런치로 복귀합니다.
-git checkout %branch%
+echo 개발 브런치로 체크아웃합니다.
+git checkout develop
 
-
-echo 릴리즈 버전을 머지합니다.
+echo 최신버전을 develop 브런치에 반영합니다.
 git merge release/%version%
+git push
 
-
-
-
-
+echo 브런치를 원래 브런치로 변경합니다.
+git checkout %featBranch%
