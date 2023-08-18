2d7e7cca848094e707f6d3b636c6cf58670abd1f
Soung jin Ryu <neocat@kakao.com>
2023-08-18 18:48:19 +0900

    Fix. 닉네임변경

--- src/interactions/app/닉네임변경.ts
+++ src/interactions/app/닉네임변경.ts
@@ -3,7 +3,7 @@ import { appInteraction } from 'interactions/app';
 import { basename } from 'path';
 
 import authTokenSelect from 'components/authTokenSelect';
-import { getUser } from 'components/twitch';
+import { setUserNick } from 'components/discordUser';
 
 const name = basename(__filename, __filename.endsWith('js') ? '.js' : '.ts');
 const type = ApplicationCommandType.User;
@@ -15,19 +15,14 @@ export const exec = (interaction: appInteraction) => {
     authTokenSelect(target_id, 'select nick', 2, 3)
         .then(async user => {
             if (Array.isArray(user)) {
-                // 선택지
-                interaction.re({
-                    components: user,
-                });
+                await interaction.re({ components: user });
             } else {
-                const [user_profile] = await getUser(target_id);
-                const { display_name, login } = user_profile;
-                // TODO: 닉네임 변경 선택지 구현
+                await setUserNick(target_id, user.user_id);
             }
         })
         .catch(err => {
             interaction.re({
-                content: '해당하는 사용자는 로그인을 하지 않았어요!',
+                content: '해당하는 사용자는 로그인을 하지 않았거나, 관리자에욧!!!',
             });
         });
 };
