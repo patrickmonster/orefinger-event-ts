144c37ff6dc9f6899d823b1f35eb9d00426018f1
Soung jin Ryu <neocat@kakao.com>
2023-11-02 16:46:56 +0900

    Add. action row 하위옵션 수정

--- src/interactions/menu/component_action_row/edit.ts
+++ src/interactions/menu/component_action_row/edit.ts
@@ -1,3 +1,5 @@
+import { UpdateYNConnection, updateComponentActionRowConnect } from 'controllers/component';
+import { APIStringSelectComponent } from 'discord-api-types/v10';
 import { MessageMenuInteraction } from 'interactions/message';
 
 /**
@@ -18,6 +20,28 @@ export const exec = async (interaction: MessageMenuInteraction, component_id: st
 
     switch (target) {
         case 'option': {
+            const { values } = interaction;
+            // 컴포넌트 하위 옵션 변경
+            try {
+                const componentActionLow = components?.find(component =>
+                    component.components.find(component => 'custom_id' in component && component?.custom_id === custom_id)
+                );
+                if (!componentActionLow) throw new Error('컴포넌트를 찾을 수 없습니다.');
+                const componentsMenu = componentActionLow.components[0] as APIStringSelectComponent;
+
+                await updateComponentActionRowConnect(
+                    component_id,
+                    componentsMenu.options.map(({ value }) => ({
+                        option_id: parseInt(value),
+                        value: values.includes(value) ? 'Y' : 'N',
+                    })) as UpdateYNConnection[]
+                );
+
+                interaction.reply({ content: '옵션 변경에 성공했습니다.', ephemeral: true });
+            } catch (error) {
+                interaction.reply({ content: '옵션 변경에 실패했습니다.', ephemeral: true });
+            }
+            break;
         }
         default:
             interaction.reply({ content: '컴포넌트 수정에 실패했습니다.', ephemeral: true });
