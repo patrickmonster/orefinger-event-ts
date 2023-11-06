08e5d20e1bb311ac384b6abf94ebeeef7bd80f12
Soung jin Ryu <neocat@kakao.com>
2023-11-06 16:35:12 +0900

    Fix. prettier 설정 변경

--- src/utils/discord/component.ts
+++ src/utils/discord/component.ts
@@ -14,25 +14,37 @@ import {
 
 export const createButton = (
     custom_id: string,
-    props: Omit<APIButtonComponentBase<ButtonStyle.Primary | ButtonStyle.Secondary | ButtonStyle.Success | ButtonStyle.Danger>, 'type'>
+    props: Omit<
+        APIButtonComponentBase<ButtonStyle.Primary | ButtonStyle.Secondary | ButtonStyle.Success | ButtonStyle.Danger>,
+        'type'
+    >
 ): APIButtonComponentWithCustomId => ({
     type: ComponentType.Button,
     custom_id,
     ...props,
 });
 
-export const createPrimaryButton = (custom_id: string, props: Omit<APIButtonComponentBase<ButtonStyle.Primary>, 'type' | 'style' | 'custom_id'>) =>
-    createButton(custom_id, { style: ButtonStyle.Primary, ...props });
+export const createPrimaryButton = (
+    custom_id: string,
+    props: Omit<APIButtonComponentBase<ButtonStyle.Primary>, 'type' | 'style' | 'custom_id'>
+) => createButton(custom_id, { style: ButtonStyle.Primary, ...props });
 export const createSecondaryButton = (
     custom_id: string,
     props: Omit<APIButtonComponentBase<ButtonStyle.Secondary>, 'type' | 'style' | 'custom_id'>
 ) => createButton(custom_id, { style: ButtonStyle.Secondary, ...props });
-export const createSuccessButton = (custom_id: string, props: Omit<APIButtonComponentBase<ButtonStyle.Success>, 'type' | 'style' | 'custom_id'>) =>
-    createButton(custom_id, { style: ButtonStyle.Success, ...props });
-export const createDangerButton = (custom_id: string, props: Omit<APIButtonComponentBase<ButtonStyle.Danger>, 'type' | 'style' | 'custom_id'>) =>
-    createButton(custom_id, { style: ButtonStyle.Danger, ...props });
+export const createSuccessButton = (
+    custom_id: string,
+    props: Omit<APIButtonComponentBase<ButtonStyle.Success>, 'type' | 'style' | 'custom_id'>
+) => createButton(custom_id, { style: ButtonStyle.Success, ...props });
+export const createDangerButton = (
+    custom_id: string,
+    props: Omit<APIButtonComponentBase<ButtonStyle.Danger>, 'type' | 'style' | 'custom_id'>
+) => createButton(custom_id, { style: ButtonStyle.Danger, ...props });
 
-export const createUrlButton = (url: string, props: Omit<APIButtonComponentBase<ButtonStyle.Link>, 'type' | 'style'>): APIButtonComponentWithURL => {
+export const createUrlButton = (
+    url: string,
+    props: Omit<APIButtonComponentBase<ButtonStyle.Link>, 'type' | 'style'>
+): APIButtonComponentWithURL => {
     return {
         type: ComponentType.Button,
         style: ButtonStyle.Link,
@@ -73,8 +85,12 @@ export const createTextInput = (
     };
 };
 
-export const createTextParagraphInput = (custom_id: string, props: Omit<APITextInputComponent, 'type' | 'style' | 'custom_id'>) =>
-    createTextInput(custom_id, TextInputStyle.Paragraph, props);
+export const createTextParagraphInput = (
+    custom_id: string,
+    props: Omit<APITextInputComponent, 'type' | 'style' | 'custom_id'>
+) => createTextInput(custom_id, TextInputStyle.Paragraph, props);
 
-export const createTextShortInput = (custom_id: string, props: Omit<APITextInputComponent, 'type' | 'style' | 'custom_id'>) =>
-    createTextInput(custom_id, TextInputStyle.Short, props);
+export const createTextShortInput = (
+    custom_id: string,
+    props: Omit<APITextInputComponent, 'type' | 'style' | 'custom_id'>
+) => createTextInput(custom_id, TextInputStyle.Short, props);
