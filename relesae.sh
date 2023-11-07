45e6555bc32c0a23b24a36037679b0ffc49912bc
Soung jin Ryu <neocat@kakao.com>
2023-11-08 02:48:55 +0900

    Fix. 버전관리도구 수정

--- relesae.sh
+++ relesae.sh
@@ -14,9 +14,8 @@ fi
 echo "현재 브런치를 커밋합니다."
 git push
 
-# 개발 브런치로 체크아웃
-echo "개발 브런치로 체크아웃합니다."
-git checkout develop
+echo "마스터 브런치로 체크아웃합니다."
+git checkout master
 git pull
 
 # 현재 패키지 버전을 확인합니다.
@@ -28,9 +27,9 @@ patch="${VERSION_PARTS[2]}"
 
 echo "======================"
 echo "버전 업데이트 설정 - $version"
-echo "1. Major 버전 업데이트 [$major] (주요 업데이트)"
-echo "2. Minor 버전 업데이트 [$minor] (기능 업데이트)"
-echo "3. Patch 버전 업데이트 [$patch] (버그 수정)"
+echo "1. Major 버전 업데이트 [$major] ( - )"
+echo "2. Minor 버전 업데이트 [$minor] (주요 업데이트)"
+echo "3. Patch 버전 업데이트 [$patch] (핫 픽스)"
 echo "0. 버전 업데이트 취소"
 echo "======================"
 
@@ -57,27 +56,41 @@ case $idx in
 esac
 
 newVersion="$major.$minor.$patch"
+# 버전이 변경된 경우
 
-echo "배포를 위한 환경을 제작합니다."
+echo "배포를 위한 환경을 제작합니다. - $newVersion"
+# 마스터에서 배포 브런치를 생성합니다.
 git branch "release/$newVersion"
+# 배포 브런치로 체크아웃합니다.
 git checkout "release/$newVersion"
 
+# 패키지 버전을 변경합니다.
 npm pkg set version="$newVersion"
 
-echo "$(date '+%Y%m%d')] CreateVersion - $newVersion" >> release.log
-
-git add package.json release.log
+git add package.json
 git commit -m "release: $newVersion"
 
-git tag "release-$newVersion"
-git push origin "release/$newVersion"
+# 
+open "https://github.com/patrickmonster/orefinger-event-ts/compare/master...release/$newVersion"
 
-echo "개발 브런치로 체크아웃합니다."
-git checkout develop
-git merge "release/$newVersion"
-git push
+
+read -p "현재 브런치를 병합 하시겠습니까? 승인(엔터)/종료(다른키)" input
+
+if [[ -z "$input" ]]; then
+    echo "브런치를 병합합니다."
+    # git checkout $featBranch
+    git merge $featBranch
+    git push
+fi
+
+# git tag "release-$newVersion"
+# git push origin "release/$newVersion"
+
+# echo "개발 브런치로 체크아웃합니다."
+# git checkout develop
+# git merge "release/$newVersion"
+# git push
 
 echo "브런치를 원래 브런치로 변경합니다."
 git checkout $featBranch
 
-open "https://github.com/patrickmonster/orefinger-event-ts/compare/master...release/$newVersion"
