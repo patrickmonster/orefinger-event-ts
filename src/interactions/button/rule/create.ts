4de87d8704d3fa4cac56ac8746785658b352f0e2
Soung jin Ryu <neocat@kakao.com>
2023-12-26 18:29:02 +0900

    Add. 기본메세지 추가

--- src/interactions/button/rule/create.ts
+++ src/interactions/button/rule/create.ts
@@ -10,7 +10,11 @@ import { MessageInteraction } from 'interactions/message';
  */
 export const exec = async (interaction: MessageInteraction, auth_type: string) => {
     const { guild_id } = interaction;
-    const { insertId } = await upsertEmbedUser({ use_yn: 'N' });
+    const { insertId } = await upsertEmbedUser({
+        use_yn: 'N',
+        title: '규칙을 확인해주세요!',
+        description: `해당 서버는, 2단계(OAuth2.0)인증을 지원하여 쾌적환 환경을 만들기 위하여 노력 하고 있습니다!`,
+    });
 
     if (!guild_id) return;
 
