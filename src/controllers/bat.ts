12386e76a456863465b04aada292ca4c88b4e8a7
Soung jin Ryu <neocat@kakao.com>
2024-02-07 00:26:44 +0900

    Fix. 등록된 알림은 insert 건너뜀

--- src/controllers/bat.ts
+++ src/controllers/bat.ts
@@ -37,6 +37,21 @@ GROUP BY hash_id
 export const insertVideoEvents = async (notice_id: number, video_id: string, title: string) =>
     query(`INSERT INTO notice_video (video_id, title, notice_id) VALUES(?)`, [video_id, title, notice_id]);
 
+export const selectVideoEvents = async (notice_id: number) =>
+    query<{
+        video_id: string;
+        title: string;
+        create_at: string;
+    }>(
+        `
+SELECT video_id, title, create_at  
+FROM notice_video
+WHERE 1=1
+AND notice_id = ?
+	`,
+        notice_id
+    );
+
 export const insertLiveEvents = async (notice_id: number, id: string) =>
     query(`INSERT INTO notice_live SET ?`, {
         notice_id,
