41b22bf1ed0df06928a22d74e16a8f19bedc3aee
Soung jin Ryu <neocat@kakao.com>
2024-02-29 21:23:03 +0900

    Fix. 아프리카 메세지

--- src/bat/afreeca.ts
+++ src/bat/afreeca.ts
@@ -1,4 +1,4 @@
-import { insertLiveEvents, selectEventBats, updateLiveEvents } from 'controllers/bat';
+import { selectEventBats } from 'controllers/bat';
 import { deleteNoticeChannel } from 'controllers/notice';
 import { APIEmbed } from 'discord-api-types/v10';
 import { NoticeChannel } from 'interfaces/notice';
@@ -31,7 +31,7 @@ const convertVideoObject = (videoObject: Content, name?: string): APIEmbed => {
         title: title || 'LIVE ON',
         url: `https://play.afreecatv.com/${user_id}/${broad_no}`,
         image: {
-            url: `//liveimg.afreecatv.com/m/${broad_no}?${randomIntegerInRange(100, 999)}`,
+            url: `https://liveimg.afreecatv.com/m/${broad_no}?${randomIntegerInRange(100, 999)}`,
         },
         author: {
             name: name ?? channelName,
@@ -64,24 +64,24 @@ const getChannelLive = async (noticeId: number, hashId: string, lastId: string |
                     profile_image, // 프로필 이미지
                 } = content;
 
-                if (broad) {
-                    // 온라인
-                    const { broad_no } = broad;
-                    if (lastId === broad_no) {
-                        return reject(null);
-                    } else {
-                        await insertLiveEvents(noticeId, broad_no);
-                    }
-                } else {
-                    // 오프라인
-                    if (lastId || lastId != '0') {
-                        const result = await updateLiveEvents(noticeId);
-                        if (result.changedRows == 0) {
-                            // 이미 처리된 알림
-                            return reject(null);
-                        }
-                    }
-                }
+                // if (broad) {
+                //     // 온라인
+                //     const { broad_no } = broad;
+                //     if (lastId === broad_no) {
+                //         return reject(null);
+                //     } else {
+                //         await insertLiveEvents(noticeId, broad_no);
+                //     }
+                // } else {
+                //     // 오프라인
+                //     if (lastId || lastId != '0') {
+                //         const result = await updateLiveEvents(noticeId);
+                //         if (result.changedRows == 0) {
+                //             // 이미 처리된 알림
+                //             return reject(null);
+                //         }
+                //     }
+                // }
                 resolve(content);
             })
             .catch(reject);
@@ -119,11 +119,12 @@ const interval = async () => {
             try {
                 const liveStatus = await getChannelLive(notice_id, hash_id, id);
                 if (liveStatus) {
+                    console.log('?', convertVideoObject(liveStatus, name));
                     // online
-                    sendChannels(channels, {
-                        content: message,
-                        embeds: [convertVideoObject(liveStatus, name)],
-                    });
+                    // sendChannels(channels, {
+                    //     content: message,
+                    //     embeds: [convertVideoObject(liveStatus, name)],
+                    // });
                 } else {
                     // offline
                 }
@@ -141,11 +142,11 @@ const interval = async () => {
     console.log('탐색 :: Afreeca', new Date(), pageIndex);
 };
 
-const intervalIdx = setInterval(interval, 1000 * 60 * 5); // 5분마다 실행
-console.log('Afreeca Batch Start!');
-// interval();
+// const intervalIdx = setInterval(interval, 1000 * 60 * 5); // 5분마다 실행
+// console.log('Afreeca Batch Start!');
+interval();
 
-process.on('SIGINT', function () {
-    console.log('Afreeca Batch STOP!');
-    clearInterval(intervalIdx);
-});
+// process.on('SIGINT', function () {
+//     console.log('Afreeca Batch STOP!');
+//     clearInterval(intervalIdx);
+// });
