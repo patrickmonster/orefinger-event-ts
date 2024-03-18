11835ebff7f33cd4daebc3914ca19cf9821f1cce
Soung jin Ryu <neocat@kakao.com>
2024-03-18 15:10:23 +0900

    Fix. 유튜브 트래픽 대응

--- src/bat/youtube.ts
+++ src/bat/youtube.ts
@@ -1,91 +1,11 @@
-import axios from 'axios';
 import { sendChannels } from 'components/notice';
-import { insertVideoEvents, selectEventBats, selectVideoEvents } from 'controllers/bat';
+import { getChannelVideos } from 'components/youtubeUser';
+import { selectEventBats } from 'controllers/bat';
 import sleep from 'utils/sleep';
-import { parseString } from 'xml2js';
 
 const ERROR = (...e: any) => {
     console.error(__filename, ' Error: ', ...e);
 };
-/**
- * xml 형태의 데이터를 embed 형태로 변환합니다
- * @param video_object
- * @returns
- */
-const convertVideoObject = (video_object: any) => {
-    const {
-        'media:group': [
-            {
-                'media:title': [title],
-                'media:content': [{ $: content }],
-                'media:thumbnail': [{ $: thumbnail }],
-            },
-        ],
-    } = video_object;
-
-    return {
-        title,
-        url: content.url,
-        image: { ...thumbnail },
-        thumbnail: {
-            url: 'https://cdn.discordapp.com/attachments/682449668428529743/1125234663045201950/yt_icon_rgb.png',
-        },
-        footer: {
-            text: '제공. Youtube',
-        },
-    };
-};
-
-/**
- * 채널의 비디오 목록을 가져옵니다
- * @param notice_id
- * @param hash_id
- * @returns
- */
-const getChannelVideos = async (notice_id: number, hash_id: string) =>
-    new Promise<{
-        videos: any[];
-        channel_title: string;
-    }>((resolve, reject) => {
-        axios
-            .get(`https://www.youtube.com/feeds/videos.xml?channel_id=${hash_id}`)
-            .then(({ data }) => {
-                parseString(data, async (err, data) => {
-                    if (err) return reject(err);
-                    const {
-                        feed: {
-                            title: [channel_title],
-                            entry,
-                        },
-                    } = data;
-
-                    const oldVideos = await selectVideoEvents(notice_id);
-
-                    const videos = [];
-                    for (const video_object of entry) {
-                        const {
-                            id: [id],
-                            'media:group': [
-                                {
-                                    'media:title': [title],
-                                },
-                            ],
-                        } = video_object;
-                        // 이미 등록된 비디오는 건너뜁니다 (중복 방지)
-                        if (oldVideos.find(v => v.video_id === id)) continue;
-
-                        try {
-                            await insertVideoEvents(notice_id, id, title);
-                            videos.push(convertVideoObject(video_object));
-                        } catch (e) {
-                            continue;
-                        }
-                    }
-                    resolve({ videos, channel_title });
-                });
-            })
-            .catch(reject);
-    });
 
 // 5분마다 실행되는 함수
 const interval = async () => {
