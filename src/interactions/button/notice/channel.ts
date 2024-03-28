149460bc34f8807f63a62311b96a9d782b057b4d
Soung jin Ryu <neocat@kakao.com>
2024-03-28 13:06:22 +0900

    Fix. 알림 테스트 추가

--- src/interactions/button/notice/channel.ts
+++ src/interactions/button/notice/channel.ts
@@ -1,5 +1,7 @@
+import { sendTestNotice } from 'components/notice';
 import { selectNoticeDetailEditByModel } from 'controllers/notice';
 import { MessageInteraction } from 'interactions/message';
+import { createActionRow, createUrlButton } from 'utils/discord/component';
 
 /**
  *
@@ -7,6 +9,7 @@ import { MessageInteraction } from 'interactions/message';
  * @param interaction
  */
 export const exec = async (interaction: MessageInteraction, noticeId: string, modeType: string) => {
+    const { guild_id } = interaction;
     switch (modeType) {
         case 'edit': {
             const model = await selectNoticeDetailEditByModel(noticeId);
@@ -15,6 +18,39 @@ export const exec = async (interaction: MessageInteraction, noticeId: string, mo
                 ...model,
                 custom_id: `notice edit ${noticeId}`,
             });
+        }
+        case 'test': {
+            interaction.differ({ ephemeral: true });
+            if (!guild_id) {
+                return interaction.reply({
+                    content: '서버 정보를 찾을 수 없습니다.',
+                    ephemeral: true,
+                });
+            }
+            try {
+                await sendTestNotice(noticeId, guild_id);
+            } catch (e) {
+                return interaction.reply({
+                    content: '알림 전송에 실패하였습니다. \n 하단 문서를 확인해 주세요',
+                    ephemeral: true,
+                    components: [
+                        createActionRow(
+                            createUrlButton('https://orefinger.notion.site/e5856c2d3e6f494e95e1ea5a927f31bf', {
+                                label: '문서 확인',
+                                emoji: {
+                                    name: '📌',
+                                },
+                            })
+                        ),
+                    ],
+                });
+            }
+
+            interaction.reply({
+                content: '알림이 전송되었습니다.',
+                ephemeral: true,
+            });
+            break;
         }
     }
 };
