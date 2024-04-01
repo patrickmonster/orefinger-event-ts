80ce77ab31db016b4be594cb7f419dcd74dae415
Soung jin Ryu <neocat@kakao.com>
2024-04-01 18:14:35 +0900

    Add. 통계용 쿼리 추가

--- src/controllers/main.ts
+++ src/controllers/main.ts
@@ -1,3 +1,35 @@
-import { query } from 'utils/database';
+import getConnection from 'utils/database';
 
-export default () => query('select 1');
+export const getTotal = async () =>
+    getConnection(async query => {
+        const total = await query<{
+            total: number;
+        }>(`SELECT COUNT(1) AS total FROM auth`).then(l => l[0].total);
+
+        const list = await query<{
+            dd: string;
+            count: number;
+        }>(`
+SELECT 
+    at2.auth_type
+    , at2.tag 
+    , at2.tag_kr 
+    , DATE_FORMAT(vat.create_at , "%y-%m-%d") AS create_to
+    , COUNT(1) AS counts 
+FROM auth_type at2 
+INNER JOIN v_auth_token vat
+    ON vat.type = at2.auth_type 
+    AND vat.create_at > DATE_ADD(NOW(), INTERVAL -10 DAY)
+WHERE 1=1
+AND (
+    at2.use_yn = 'Y'
+    OR at2.auth_type = 1
+)
+GROUP BY create_to, at2.auth_type 
+ORDER BY create_to desc 
+        `);
+        return {
+            total,
+            list,
+        };
+    });
