266f0204b6fdc8611e77fdd51c6e42332250d451
Soung jin Ryu <neocat@kakao.com>
2024-04-12 16:14:43 +0900

    Fix. 토탈로 변경

--- src/utils/serverState.ts
+++ src/utils/serverState.ts
@@ -1,9 +1,12 @@
 export const bootTime = Date.now();
 
-export let serverRequset = 0;
+export let lastServerRequset = 0;
+
+let serverRequset = 0;
 export const addServerRequest = () => serverRequset++;
 
 const timmer = setInterval(() => {
+    lastServerRequset = serverRequset;
     serverRequset = 0;
 }, 1000 * 60);
 
