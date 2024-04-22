582e29a85774059f69e9906f7bb9e0a6a319ff79
Soung jin Ryu <neocat@kakao.com>
2024-04-22 17:58:39 +0900

    Del. ws모듈 삭제 (우선 필요없음)

--- src/routes/websocket/ws.ts
+++
@@ -1,7 +0,0 @@
-import { FastifyInstance } from 'fastify';
-
-export default async (fastify: FastifyInstance, opts: any) => {
-    fastify.get('/ws', { websocket: true }, (connection, req) => {
-        //
-    });
-};
