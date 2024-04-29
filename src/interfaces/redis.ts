a9e85fa8ddc7e464fd016e1e455791a1dcbd06ed
Soung jin Ryu <neocat@kakao.com>
2024-04-30 08:55:54 +0900

    Fix. 명령어 수정 / 대 -> 소문자로 변경

---
+++ src/interfaces/redis.ts
@@ -0,0 +1,20 @@
+export type ECSState = 'channels' | 'join' | 'connect' | 'leave';
+export type LiveState = 'online' | 'offline' | 'change';
+
+export interface BaseState {
+    revision: string;
+    id: string;
+}
+
+export interface LiveStateMessage {
+    targetId?: string;
+    noticeId: number;
+    hashId: string;
+    liveStatus: any;
+}
+
+export interface ECSStateMessage {
+    count: number;
+    userCount: number;
+    hash_id?: string;
+}
