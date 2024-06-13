46495dbce90a69c3a547ab8c89dce5eba4b777d3
Soung jin Ryu <neocat@kakao.com>
2024-06-13 09:37:52 +0900

    Add. 안내문구 추가

--- src/interactions/command/알림/설정.ts
+++ src/interactions/command/알림/설정.ts
@@ -13,6 +13,26 @@ export const exec = async (interaction: AppChatInputInteraction, selectOption: S
 
     interaction.reply({
         ephemeral: true,
+        content: '설정하실 알림을 선택해주세요.',
+        embeds: [
+            {
+                title: '혹시... 설정이 어려우신가요?',
+                description: `
+방송알리미는 여러분의 방송을 조금 더 간편하게 등록 가능하도록
+"알림 신규" 기능을 추가로 제공하고 있습니다.
+
+\`/알림 신규 링크:url\` 명령어를 통해 플렛폼 알림을 등록 할 수 있습니다.
+
+- 치지직: https://chzzk.naver.com/...
+- 아프리카: https://[bj,play,www].afreecatv.com/...
+- 유튜브: https://www.youtube.com/@채널명
+                `,
+                image: {
+                    url: 'https://cdn.orefinger.click/upload/466950273928134666/b70160c5-036d-4c8a-8336-403dabf88ef1.png',
+                },
+                color: 0x00ff00,
+            },
+        ],
         components: await createComponentSelectMenuByComponentPagingMenuByKey(
             {
                 custom_id: 'notice list',
