3d811d2008f5dfae6aab4d7798917289cf9ac498
Soung jin Ryu <neocat@kakao.com>
2024-07-23 01:38:32 +0900

    Add. 길드 초대 리스트

--- src/routes/admin/discord/guild.ts
+++ src/routes/admin/discord/guild.ts
@@ -1,4 +1,4 @@
-import { getGuild } from 'components/discord';
+import { getGuild, getGuildInvites } from 'components/discord';
 import { FastifyInstance } from 'fastify';
 
 import discord from 'utils/discordApiInstance';
@@ -29,5 +29,27 @@ export default async (fastify: FastifyInstance, opts: any) => {
                 getGuild(req.params.guild_id),
                 discord.get(`/guilds/${req.params.guild_id}/channels`),
             ]).then(([guild, channels]) => ({ guild, channels }))
+    );
+
+    fastify.get<{
+        Params: { guild_id: string };
+    }>(
+        '/guild/:guild_id/invites',
+        {
+            onRequest: [fastify.masterkey],
+            schema: {
+                security: [{ Master: [] }],
+                description: '길드 초대 조회 \n - https://discord.com/developers/docs/resources/invite',
+                tags: ['Admin'],
+                deprecated: false,
+                params: {
+                    type: 'object',
+                    properties: {
+                        guild_id: { type: 'string' },
+                    },
+                },
+            },
+        },
+        async req => getGuildInvites(req.params.guild_id)
     );
 };
