99fe79b32eade9b33de7dd7194f9396a863f9d89
Soung jin Ryu <neocat@kakao.com>
2024-08-03 13:08:55 +0900

    Del. 훅 제거

--- src/routes/notice/webhook.ts
+++ src/routes/notice/webhook.ts
@@ -9,31 +9,6 @@ export default async (fastify: FastifyInstance, opts: any) => {
     }>(
         '/webhook/:channel',
         {
-            onRequest: [fastify.authenticate],
-            schema: {
-                security: [{ Bearer: [] }],
-                description: '훅을 생성합니다.',
-                summary: '훅 생성',
-                tags: ['Notice'],
-                deprecated: false,
-                querystring: {
-                    type: 'object',
-                    required: ['name'],
-                    properties: {
-                        name: { type: 'string', description: '훅 이름' },
-                    },
-                },
-            },
-        },
-        async req => webhookCreate(req.params.channel, { name: req.query.name, auth_id: req.user.id })
-    );
-
-    fastify.post<{
-        Params: { channel: string };
-        Querystring: { name: string };
-    }>(
-        '/webhook/:channel',
-        {
             onRequest: [fastify.masterkey],
             schema: {
                 security: [{ Bearer: [] }],
