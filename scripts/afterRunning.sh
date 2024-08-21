f356d2affc90b83db60b70910cb999c92c9b8226
Soung jin Ryu <neocat@kakao.com>
2024-08-21 09:50:24 +0900

    Fix. 환경변수 수정

--- scripts/afterRunning.sh
+++ scripts/afterRunning.sh
@@ -20,17 +20,32 @@ rm $filename
 
 
 # .env 파일 생성
-echo DB_USER=$(aws ssm get-parameters --region ap-northeast-2 --names $path"DB_USER" --query Parameters[0].Value | sed 's/"//g') >> $filename
+echo BUCKET=$(aws ssm get-parameters --region ap-northeast-2 --names $path"BUCKET" --query Parameters[0].Value | sed 's/"//g')  >> $filename
+
+echo DB_USER=$(aws ssm get-parameters --region ap-northeast-2 --names $path"DB_USER" --query Parameters[0].Value | sed 's/"//g')  >> $filename
 echo DB_HOST=$(aws ssm get-parameters --region ap-northeast-2 --names $path"DB_HOST" --query Parameters[0].Value | sed 's/"//g')  >> $filename
 echo DB_PASSWD=$(aws ssm get-parameters --region ap-northeast-2 --names $path"DB_PASSWD" --query Parameters[0].Value | sed 's/"//g')  >> $filename
 echo DB_DATABASE=$(aws ssm get-parameters --region ap-northeast-2 --names $path"DB_DATABASE" --query Parameters[0].Value | sed 's/"//g')  >> $filename
 
 echo REDIS_URL=$(aws ssm get-parameters --region ap-northeast-2 --names $path"REDIS_URL" --query Parameters[0].Value | sed 's/"//g')  >> $filename
 
+echo DISCORD_CLIENT_ID=$(aws ssm get-parameters --region ap-northeast-2 --names $path"DISCORD_CLIENT_ID" --query Parameters[0].Value | sed 's/"//g')  >> $filename
+echo DISCORD_PUBLIC_KEY=$(aws ssm get-parameters --region ap-northeast-2 --names $path"DISCORD_PUBLIC_KEY" --query Parameters[0].Value | sed 's/"//g')  >> $filename
+echo DISCORD_TOKEN=$(aws ssm get-parameters --region ap-northeast-2 --names $path"DISCORD_TOKEN" --query Parameters[0].Value | sed 's/"//g')  >> $filename
+
+echo JWT_SECRET=$(aws ssm get-parameters --region ap-northeast-2 --names $path"JWT_SECRET" --query Parameters[0].Value | sed 's/"//g')  >> $filename
+echo MASTER_KEY=$(aws ssm get-parameters --region ap-northeast-2 --names $path"MASTER_KEY" --query Parameters[0].Value | sed 's/"//g')  >> $filename
+
+echo KAKAO_CLIENT=$(aws ssm get-parameters --region ap-northeast-2 --names $path"KAKAO_CLIENT" --query Parameters[0].Value | sed 's/"//g')  >> $filename
+echo KAKAO_SECRET=$(aws ssm get-parameters --region ap-northeast-2 --names $path"KAKAO_SECRET" --query Parameters[0].Value | sed 's/"//g')  >> $filename
+
+echo TOSS_CLIENT=$(aws ssm get-parameters --region ap-northeast-2 --names $path"TOSS_CLIENT" --query Parameters[0].Value | sed 's/"//g')  >> $filename
+echo TOSS_SECRET=$(aws ssm get-parameters --region ap-northeast-2 --names $path"TOSS_SECRET" --query Parameters[0].Value | sed 's/"//g')  >> $filename
+
+echo YOUTUBE_API_KEY=$(aws ssm get-parameters --region ap-northeast-2 --names $path"YOUTUBE_API_KEY" --query Parameters[0].Value | sed 's/"//g')  >> $filename
+
 echo WEB_HOOK_URL=$(aws ssm get-parameters --region ap-northeast-2 --names $path"WEB_HOOK_URL" --query Parameters[0].Value | sed 's/"//g')  >> $filename
 
-echo DISCORD_TOKEN=$(aws ssm get-parameters --region ap-northeast-2 --names $path"DISCORD_TOKEN" --query Parameters[0].Value | sed 's/"//g')  >> $filename
-echo YOUTUBE_API_KEY=$(aws ssm get-parameters --region ap-northeast-2 --names $path"YOUTUBE_API_KEY" --query Parameters[0].Value | sed 's/"//g')  >> $filename
 
 # npm 설치
 sudo /usr/bin/yarn
