ce867f5debbbaaf353c8ee3b02f6ec1f1c9eff61
Soung jin Ryu <neocat@kakao.com>
2024-08-22 09:25:39 +0900

    Add. port

--- scripts/afterRunning.sh
+++ scripts/afterRunning.sh
@@ -46,6 +46,7 @@ echo YOUTUBE_API_KEY=$(aws ssm get-parameters --region ap-northeast-2 --names $p
 
 echo WEB_HOOK_URL=$(aws ssm get-parameters --region ap-northeast-2 --names $path"WEB_HOOK_URL" --query Parameters[0].Value | sed 's/"//g')  >> $filename
 
+echo PORT=3000  >> $filename
 
 # npm 설치
 sudo /usr/bin/yarn
