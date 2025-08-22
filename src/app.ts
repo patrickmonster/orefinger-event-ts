189b8790b7b0c98e3ee468a14ca4c79de29bb132
Soung jin Ryu <neocat@kakao.com>
2025-08-23 08:36:29 +0900

    코드 정리: 사용하지 않는 SshTunnel 관련 코드 및 readFileSync 호출 주석 처리

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,7 @@ import helmet from '@fastify/helmet';
 import fastify from 'fastify';
 
 import { config } from 'dotenv';
-import { existsSync, readFileSync } from 'fs';
+import { existsSync } from 'fs';
 import { join } from 'path';
 import { env } from 'process';
 
@@ -26,8 +26,6 @@ import { ParseInt } from 'utils/object';
 import 'utils/procesTuning';
 import { addServerRequest } from 'utils/serverState';
 
-import { SshTunnel } from 'ssh-tunneling';
-
 const server = fastify({
     // logger: env.NODE_ENV != 'prod'
     logger: { transport: { target: '@fastify/one-line-logger' } },
@@ -38,16 +36,14 @@ const server = fastify({
 server.register(helmet, { global: true });
 server.register(Multipart);
 
-const sshTunnel = new SshTunnel({
-    host: env.TARGET_HOST || 'localhost',
-    port: 22,
-    username: env.TARGET_NAME,
-    privateKey: readFileSync(join(env.PWD || __dirname, 'db.pem')),
-});
+// const sshTunnel = new SshTunnel({
+//     host: env.TARGET_HOST || 'localhost',
+//     port: 22,
+//     username: env.TARGET_NAME,
+//     privateKey: readFileSync(join(env.PWD || __dirname, 'db.pem')),
+// });
 
-
-console.log( readFileSync(join(env.PWD || __dirname, 'db.pem')))
-
+// console.log(readFileSync(join(env.PWD || __dirname, 'db.pem')));
 
 /* 데이터 베이스 터널링을 만듬 */
 // const host = `${env.DB_HOST || 'localhost'}`;
@@ -56,24 +52,24 @@ console.log( readFileSync(join(env.PWD || __dirname, 'db.pem')))
 // env.DB_PORT = (3400 + Math.floor(Math.random() * 501)).toString();
 
 // sshTunnel.forwardOut(`${env.DB_PORT}:${host}:${port}`).then(config => {
-    // console.log('CONNECTED ::', config.id, env.DB_PORT);
+// console.log('CONNECTED ::', config.id, env.DB_PORT);
 
-    server.register(AutoLoad, { dir: join(__dirname, 'plugins') });
-    // 라우터
-    server.register(AutoLoad, { dir: join(__dirname, 'routes'), ignorePattern: /.*(test|spec|interface).*/ });
+server.register(AutoLoad, { dir: join(__dirname, 'plugins') });
+// 라우터
+server.register(AutoLoad, { dir: join(__dirname, 'routes'), ignorePattern: /.*(test|spec|interface).*/ });
 
-    server.listen({ port: ParseInt(process.env.PORT || 3000), host: '::' }, (err, address) => {
-        if (err) {
-            console.error(err);
-            process.exit(1);
-        }
-        console.log(`Server started in  ${process.uptime()}s`);
-        console.log(`Server listening at ${address}`);
-    });
-    
+server.listen({ port: ParseInt(process.env.PORT || 3000), host: '::' }, (err, address) => {
+    if (err) {
+        console.error(err);
+        process.exit(1);
+    }
+    console.log(`Server started in  ${process.uptime()}s`);
+    console.log(`Server listening at ${address}`);
+});
+
 // });
 
-process.on('SIGINT', sshTunnel.close);
+// process.on('SIGINT', sshTunnel.close);
 
 // 서버 요청 카운트
 server.addHook('onRequest', (request, reply, done) => {
