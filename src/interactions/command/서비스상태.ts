ee3e9cfd08c5cacb6899814ac30e3575986d423f
Soung jin Ryu <neocat@kakao.com>
2026-01-26 16:20:49 +0900

    수정: EC2 Task 상태 정보를 제거하고 관련 메시지 수정

--- src/interactions/command/서비스상태.ts
+++ src/interactions/command/서비스상태.ts
@@ -1,4 +1,4 @@
-import { ecsTaskState, liveState } from 'controllers/log';
+import { liveState } from 'controllers/log';
 import dayjs from 'dayjs';
 import { AppChatInputInteraction, SelectOptionType } from 'interactions/app';
 import { createChatinputCommand, createEmbed } from 'utils/discord/component';
@@ -8,14 +8,12 @@ import { catchRedis } from 'utils/redis';
 const { version } = require('../../../package.json');
 
 export const exec = async (interaction: AppChatInputInteraction, selectOption: SelectOptionType) => {
-    const { ids, time, c, notices, chnnels } = await catchRedis(
+    const { time, c, notices, chnnels } = await catchRedis(
         'server:status',
         async () => {
-            const { ids } = await ecsTaskState();
             const { time, c, notices, chnnels } = await liveState();
 
             return {
-                ids,
                 time,
                 c,
                 notices,
@@ -31,7 +29,7 @@ export const exec = async (interaction: AppChatInputInteraction, selectOption: S
                 title: '현재 상태',
                 description: `
 - Version: ${version}
-- EC2 Task: ${ids.length}개
+- EC2 Task: N/A (현재 미지원)
 - Running : ${getTimeStringSeconds(process.uptime())}
 - Live Sending delay : ${time} sec (최근3달 ${c.toLocaleString()}건 평균)
 - Channel: ${chnnels.toLocaleString()}개
@@ -40,7 +38,7 @@ ${notices.map(({ cnt, tag }) => `\t⌞ ${tag}: \t${cnt.toLocaleString()}건`).jo
 
 
 * 알림 종류별로 처리 속도가 상이할 수 있습니다.
-\`알림 처리 속도는 분당 ${(120 * ids.length).toLocaleString()}개 입니다.\`
+\`알림 처리 속도는 분당 ${(120 * 4).toLocaleString()}개 입니다.\`
                     `,
                 thumbnail: {
                     url: 'https://cdn.orefinger.click/post/466950273928134666/3ee49895-2ac5-48ba-a45c-5855a7d45ee1.png',
